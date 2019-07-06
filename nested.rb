754b0590650a780dfb0085cf43dcb07756e20bec
Luke Menard <lukemenard@gmail.com>
2019-07-06 20:59:50 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -127,6 +127,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][;languages] => ["C"], "Assembly"
+  programmer_hash[:dennis_ritchie][;languages] => {
+    ["C"], 
+    "Assembly"
+  }
   return programmer_hash
 end
